eff6ddf4083a883789c1e511ddfc5f2eb2afc08d
fernandolievano <fernandolievan@gmail.com>
2024-05-03 14:03:14 -0300

    tailwind added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Twitch Extension v0.1</h1>
+    <div className='px-8 py-4'>
+      <h1 className='text-3xl font-bold'>Twitch Extension v0.1</h1>
     </div>
   );
 }
