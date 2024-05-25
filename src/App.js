4da111b3b5eef69d70433ed38813c6caaf0f2d2a
fernandolievano <fernandolievan@gmail.com>
2024-05-24 23:59:08 -0300

    added connect button component, bg color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import LoginButton from './components/LoginButton';
 
 function App() {
   return (
-    <div className='px-8 py-4'>
-      <h1 className='text-3xl font-bold'>Twitch Extension v0.1</h1>
+    <div className='px-8 py-4 bg-[#b9a3e3] h-full w-full text-white'>
+      <h1 className='text-3xl font-bold text-center'>Twitch Extension v0.1</h1>
+
+      <LoginButton />
     </div>
   );
 }
---
+++ src/components/LoginButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginButton = () => {
+  return (
+    <div className="flex justify-center pt-8">
+      <button className='bg-[#6441a5] border-2 border-[#6441a5] px-8 py-2 rounded font-bold shadow-xl transition-colors hover:bg-[#b9a3e3] hover:border-white hover:shadow-none'>
+        CONNECT WITH TWITCH
+      </button>
+    </div>
+  );
+};
+
+export default LoginButton;
