180a4369aeaef0459dd29e545b34bb7f7de25e53
fernandolievano <fernandolievan@gmail.com>
2024-05-25 02:23:54 -0300

    button hover style

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -8,7 +8,7 @@ const LoginButton = () => {
         href={
           `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${process.env.REACT_APP_TWITCH_CLIENT_ID}&redirect_uri=http://localhost:3000&scope=user%3Aread%3Afollows`
         }
-        className='flex items-center justify-start bg-primary-color border-2 border-primary-color px-4 py-4 rounded font-bold shadow-lg transition-colors hover:bg-secondary-color hover:border-light-color hover:shadow-none'
+        className='flex items-center justify-start bg-primary-color border-2 border-primary-color px-4 py-4 rounded font-bold shadow transition-all hover:shadow-2xl hover:scale-[1.01]'
       >
         <TwitchLogo width={24} height={24} />
         <span className='pl-2 text-lg'>CONNECT WITH TWITCH</span>
