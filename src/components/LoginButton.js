ae71eb74181f4a8ce4f06a7bb00e88e21a0a58fc
fernandolievano <fernandolievan@gmail.com>
2024-05-25 02:20:49 -0300

    twitch logo component, link to login

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import TwitchLogo from './TwitchLogo';
 
 const LoginButton = () => {
   return (
     <div className="flex justify-center pt-8">
-      <button className='bg-primary-color border-2 border-primary-color px-8 py-2 rounded font-bold shadow-lg transition-colors hover:bg-secondary-color hover:border-light-color hover:shadow-none'>
-        CONNECT WITH TWITCH
-      </button>
+      <a
+        href={
+          `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${process.env.REACT_APP_TWITCH_CLIENT_ID}&redirect_uri=http://localhost:3000&scope=user%3Aread%3Afollows`
+        }
+        className='flex items-center justify-start bg-primary-color border-2 border-primary-color px-4 py-4 rounded font-bold shadow-lg transition-colors hover:bg-secondary-color hover:border-light-color hover:shadow-none'
+      >
+        <TwitchLogo width={24} height={24} />
+        <span className='pl-2 text-lg'>CONNECT WITH TWITCH</span>
+      </a>
     </div>
   );
 };
